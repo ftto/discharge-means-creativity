b33edeb613e1d86b73dfa30864ff8faacb965480
dane <dane@watcha.com>
2019-05-13 08:42:58 +0900

    WIP

--- src/App.svelte
+++ src/App.svelte
@@ -32,13 +32,24 @@
 
   .information {
     position: absolute;
-    right: 50px;
-    bottom: 50px;
+    max-width: 80%;
+    right: 2vw;
+    bottom: 2vw;
     background-color: rgba(0, 0, 0, 0.4);
     padding: 10px;
   }
 
-  h1 {
+  h1,
+  h3 {
+    margin: 0;
+    text-align: right;
+  }
+
+  h3 {
+    margin-bottom: 10px;
+  }
+
+  p.description {
     text-align: right;
   }
 
@@ -46,6 +57,16 @@
   .location {
     width: 100%;
     text-align: right;
+  }
+
+  @media only screen and (max-width: 640px) {
+    h1 {
+      font-size: 23px;
+    }
+
+    h3 {
+      font-size: 18px;
+    }
   }
 </style>
 
@@ -56,9 +77,12 @@
 <div class="main-image" />
 <div class="information">
   <h1>Discharge Means Creativity;</h1>
-  <p class="date">2019년 06월 01일 | 방전이 창작이 될 때까지</p>
+  <h3>방전이 창작이 될 때까지</h3>
+
+  <p class="date">2019년 06월 01일 | 오후 2시 ~ 🔋0% 🗓</p>
   <p class="location">
     <a href="https://place.map.kakao.com/1244159721" title="카카오맵: 닷페이스">닷페이스
       사무실</a>
+    🗺
   </p>
 </div>
--- src/Modal.svelte
+++ src/Modal.svelte
@@ -34,5 +34,8 @@
 </style>
 
 <div class="modal-dialog" on:click={clickDialog}>
-  <div class="modal">modal-body</div>
+  <div class="modal">🔋Discharge Means Creativity;는 💻🔋를 꽉꽉 채워서 모여
+    방전될 때까지 놀다(?) 🏡에 가는 모임입니다. 모임 중간에 기분 좋으면 충전기
+    👏🏼찬스👏🏼도 드려요. FTTO 멤버들이 방전을 연료로 창작을 달리는 여러분을 위해
+    핸드드립 ☕️도 제공합니다.</div>
 </div>
